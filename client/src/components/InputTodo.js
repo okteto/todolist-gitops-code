6dad15cea0859c2e6b86eed24309bc5fb444dd33
Arsh Sharma <arshsharma461@gmail.com>
2023-11-07 21:52:41 +0530

    update when the action runs
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -22,7 +22,7 @@ const InputTodo = () => {
     return (
         <Fragment>
             <h1 className="title">
-                Todolist
+                Okte<span>todo</span>
             </h1>
             <form className="app-add" onSubmit={onSubmitForm}>
                 <input
